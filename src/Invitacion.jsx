7050f995a58cfa643bc9c2275f96897da65bcb74
DeveloperManuel24 <desarrolladormanuel@gmail.com>
2025-12-13 17:31:19 -0600

    portada

--- src/Invitacion.jsx
+++ src/Invitacion.jsx
@@ -10,7 +10,7 @@ export default function Invitacion() {
       <div
         className="absolute inset-0"
         style={{
-          backgroundImage: "url('/1.jpg')",
+          backgroundImage: "url('/PORTADA.jpg')",
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
